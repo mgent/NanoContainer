916231bcad2edba543b009c39f83f69c6071976c
mgent <markgent1010@gmail.com>
2022-06-01 02:58:05 +0000

    Added support for GitHub Codespaces

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+
 # build the binary
 nasm -felf64 app.asm
 ld app.o -o app
-# build and push the container
-docker buildx build --platform linux/amd64 --build-arg LANGUAGE=en -t mgent/minimal-container:latest --push .
+
+# if specified build and push the container
+while getopts "b" opt; do
+  case $opt in
+    b)
+      docker buildx build --platform linux/amd64 --build-arg LANGUAGE=en -t mgent/minimal-container:latest --push .
+      ;;
+  esac
+done
