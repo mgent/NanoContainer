84e7911821d28b054ba97237af7643ccfff27684
mgent <markgent1010@gmail.com>
2022-06-01 01:43:15 +0100

    Update build.sh
    Added the command to build and push the container

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# build the binary
 nasm -felf64 app.asm
 ld app.o -o app
-docker build .
+# build and push the container
+docker buildx build --platform linux/amd64 --build-arg LANGUAGE=en -t mgent/minimal-container:latest --push .
